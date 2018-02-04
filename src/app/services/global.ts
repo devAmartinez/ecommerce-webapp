1ba77b098188834513749099515d40cd300d3ffa
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-02-04 12:54:21 -0500

    Se incluye variable global
    La variable global para reutilizar como configuraciones fijas de la webApp.
    
    Aún falta organizar las peticiones por token a la API RESTFul

---
+++ src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+	url: 'http://localhost:3100/api/',
+	token: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YTZiNjBiNGU4OWRlZDBhYzAwNTg1NWMiLCJpYXQiOjE1MTY5ODY1NDgsImV4cCI6MTUxODE5NjE0OH0.m-7tm9WZq7W9Fql91knDUsihyvmSUitXDtoyORGu-H0'
+};

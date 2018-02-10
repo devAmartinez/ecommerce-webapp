a883ba5a3c13a9bb33e653165895b80e8735ea98
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-02-10 10:25:02 -0500

    Se ajusta el selector al estandar

--- src/app/components/error.component.ts
+++ src/app/components/error.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'error',
+	selector: 'app-error',
 	templateUrl: '../views/error.html'
 })
 
@@ -9,7 +9,7 @@ export class ErrorComponent {
 	public titulo: string;
 	public detalle: string;
 
-	constructor(){
+	constructor() {
 		this.titulo = 'Error 404 página no encontrada';
 		this.detalle = 'Por alguna razón la página solicitada no se ha encontrado.';
 	}

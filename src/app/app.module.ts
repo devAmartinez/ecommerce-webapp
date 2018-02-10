57c5de3d0a1791bb569bfbf27d05e39740b55c5f
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-02-10 10:23:32 -0500

    Se invocan nuevos componentes
    Se añaden los nuevos componentes, header, footer y los de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,32 @@ import { HttpModule  } from '@angular/http';
 import { AppComponent } from './app.component';
 
 // Componentes propios
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer.component';
 import { HomeComponent } from './components/home.component';
 import { ProductsListComponent } from './components/products-list.component';
 import { ProductAddComponent } from './components/product-add.component';
 import { ProductDetailComponent } from './components/product-detail.component';
 import { ProductEditComponent } from './components/product-edit.component';
+
+import { SignInComponent } from './components/sign-in.component';
+import { SignUpComponent } from './components/sign-up.component';
+
 import { ErrorComponent } from './components/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
     HomeComponent,
     ProductsListComponent,
     ProductAddComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    SignInComponent,
+    SignUpComponent,
     ErrorComponent
   ],
   imports: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,23 +3,32 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 // Componentes propios
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer.component';
+
 import { HomeComponent } from './components/home.component';
 import { ProductsListComponent } from './components/products-list.component';
 import { ProductAddComponent } from './components/product-add.component';
 import { ProductDetailComponent } from './components/product-detail.component';
 import { ProductEditComponent } from './components/product-edit.component';
+
+import { SignInComponent } from './components/sign-in.component';
+import { SignUpComponent } from './components/sign-up.component';
+
 import { ErrorComponent } from './components/error.component';
 
-const appRoutes:Routes = [
-	{path: '', component: HomeComponent},
-	{path: 'home', component: HomeComponent},
-	{path: 'products', component: ProductsListComponent},
-	{path: 'add-product', component: ProductAddComponent},
-	{path: 'product-detail/:id', component: ProductDetailComponent},
-	{path: 'product-edit/:id', component: ProductEditComponent},
-	{path: '**', component: ErrorComponent}
+const appRoutes: Routes = [
+  {path: '',	component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsListComponent},
+  {path: 'add-product', component: ProductAddComponent},
+  {path: 'product-detail/:id', component: ProductDetailComponent},
+  {path: 'product-edit/:id', component: ProductEditComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
-export const appRoutingProviders:any[] = [];
+export const appRoutingProviders: any[] = [];
 
-export const routing:ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
